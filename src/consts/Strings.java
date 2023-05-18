55ea0871049c5f5a94372c071d7e612b0b73a88d
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-05-18 20:53:15 +0300

    Update Strings.java  Rename application title from "Sudoku Solver" to "Super Sudoku Solver" to match repository name

--- src/consts/Strings.java
+++ src/consts/Strings.java
@@ -2,7 +2,7 @@ package consts;
 
 public final class Strings {
 
-	public static final String APP_NAME = "Sudoku Solver";
+	public static final String APP_NAME = "Super Sudoku Solver";
 	public static final String SOLVE = "Solve";
 	public static final String SKIP = "Skip";
 	public static final String STEPS_DISPLAY_PREFIX = "Steps: ";
