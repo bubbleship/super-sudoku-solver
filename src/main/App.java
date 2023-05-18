d1616cf5827964fcb5aa7f322bd9fdfaece0a574
bubbleship <84926115+bubbleship@users.noreply.github.com>
2023-05-18 21:06:45 +0300

    Update App.java  Fix code style inconsistency   * Migrate indentation from spaces to tabs   * Remove redundant line break

--- src/main/App.java
+++ src/main/App.java
@@ -8,9 +8,8 @@ import javafx.stage.Stage;
 
 public final class App extends Application {
 
-
-    public static final int WIDTH = 888;
-    public static final int HEIGHT = 592;
+	public static final int WIDTH = 888;
+	public static final int HEIGHT = 592;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -18,10 +17,10 @@ public final class App extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("solver.fxml"));
-        Scene scene = new Scene(loader.load(), WIDTH, HEIGHT);
-        stage.setScene(scene);
-        stage.setTitle(Strings.APP_NAME);
-        stage.show();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("solver.fxml"));
+		Scene scene = new Scene(loader.load(), WIDTH, HEIGHT);
+		stage.setScene(scene);
+		stage.setTitle(Strings.APP_NAME);
+		stage.show();
 	}
 }
